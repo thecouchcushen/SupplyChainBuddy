ee4a8c408a00903c62974373fe5b98736ca376a6
Liam C <thecouchcushen@gmail.com>
2023-01-21 18:12:20 -0500

    Services complete (json server)

--- frontend/src/services/pos.js
+++ frontend/src/services/pos.js
@@ -6,3 +6,27 @@ const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then((response) => response.data)
 }
+
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then((response) => response.data)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then((response) => response.data)
+}
+
+const del = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then((response) => response.data)
+}
+
+const poService = {
+    getAll,
+    create,
+    update,
+    del
+}
+
+export default poService
