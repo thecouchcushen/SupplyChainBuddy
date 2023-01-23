fb404710c3bb5edd17e28b16dfd146e43d63319c
Liam C <thecouchcushen@gmail.com>
2023-01-22 23:15:19 -0500

    SKUcatalog BOM only displays when nonzero

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,22 +15,60 @@ function App() {
   const [skus, setSkus] = useState([])
   const [suppliers, setSuppliers] = useState([])
   const [inventory, setInventory] = useState([])
+  const [currentView, setCurrentView] = useState("pos")
 
   useMount(() => inventoryService.getAll().then(response => setInventory(response)))
   useMount(() => supplierService.getAll().then(response => setSuppliers(response)))
   useMount(() => skuService.getAll().then(response => setSkus(response)))
   useMount(() => poService.getAll().then(response => setPos(response)))
+
   //console.log(inventory)
   //console.log(pos)
   //console.log(skus)
-  return (
+  //console.log(suppliers);
+  
+  if (currentView === "pos") {
+    return (
     <div>
-        <Inventory key="InventoryLevels" inventory={inventory} />
-        <Suppliers key="SupplierCatalog" suppliers={suppliers} />
-        <SkuCatalog key="SkuCatalog"  skus={skus}/>
-        <Pos key="PurchaseOrders" pos={pos} />
+      <button onClick={() => setCurrentView("pos")}>Purchase Orders</button>
+      <button onClick={() => setCurrentView("inventory")}>Inventory Levels</button>
+      <button onClick={() => setCurrentView("suppliers")}>Supplier Catalog</button>
+      <button onClick={() => setCurrentView("skus")}>SKU Catalog</button>
+      <Pos key="PurchaseOrders" pos={pos} /> 
     </div>
-  );
+    )
+  } else if (currentView === "inventory") {
+    return (
+    <div>
+      <button onClick={() => setCurrentView("pos")}>Purchase Orders</button>
+      <button onClick={() => setCurrentView("inventory")}>Inventory Levels</button>
+      <button onClick={() => setCurrentView("suppliers")}>Supplier Catalog</button>
+      <button onClick={() => setCurrentView("skus")}>SKU Catalog</button>
+      <Inventory key="InventoryLevels" inventory={inventory} /> 
+    </div>
+    )
+  } else if (currentView === "suppliers") {
+    return (
+    <div>
+      <button onClick={() => setCurrentView("pos")}>Purchase Orders</button>
+      <button onClick={() => setCurrentView("inventory")}>Inventory Levels</button>
+      <button onClick={() => setCurrentView("suppliers")}>Supplier Catalog</button>
+      <button onClick={() => setCurrentView("skus")}>SKU Catalog</button>
+      <Suppliers key="SupplierCatalog" suppliers={suppliers} /> 
+    </div>
+    )
+  } else if (currentView === "skus") {
+    return (
+    <div>
+      <button onClick={() => setCurrentView("pos")}>Purchase Orders</button>
+      <button onClick={() => setCurrentView("inventory")}>Inventory Levels</button>
+      <button onClick={() => setCurrentView("suppliers")}>Supplier Catalog</button>
+      <button onClick={() => setCurrentView("skus")}>SKU Catalog</button>
+      <SkuCatalog key="SkuCatalog"  skus={skus}/> 
+    </div>
+    )
+  }
+  
 }
 
 export default App;
--- frontend/src/components/SkuCatalog/SKU.js
+++ frontend/src/components/SkuCatalog/SKU.js
@@ -1,18 +1,31 @@
 const SkuEntry = ({sku}) => {
-    return (
+    if (sku.BOM.length > 0) {
+        return (
         <div>
             
             <h1>{sku.SKU}</h1>
             <h2>{sku.description}</h2>
             <h3>Bill of Materials:</h3>
-            {sku.BOM.map((bomItem, i) => 
+            {
+            sku.BOM.map((bomItem, i) => 
             <div key={sku.SKU + "BOMitem" + i}>
                 <p>{bomItem[0]}</p>
                 <p>Quantity: {bomItem[1]}</p>
             </div>
-            )}
+            )
+            
+            }
         </div>
-    )
+        )
+    } else {
+        return (
+            <div>
+                <h1>{sku.SKU}</h1>
+                <h2>{sku.description}</h2>
+            </div>
+        )
+    }
+
 }
 
 export default SkuEntry
