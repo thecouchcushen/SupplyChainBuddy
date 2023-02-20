a143fc8a665cadcd569398726d3f8bf5b3b4a20a
Liam C <thecouchcushen@gmail.com>
2023-02-20 17:44:46 -0500

    Added ChakraUI table for suppliers

--- frontend/src/components/Suppliers/Suppliers.js
+++ frontend/src/components/Suppliers/Suppliers.js
@@ -1,10 +1,23 @@
 import Supplier from "./Supplier"
 import { useState } from "react"
-import {AgGridReact} from 'ag-grid-react'
+//import {AgGridReact} from 'ag-grid-react'
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
 
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
 
+/*
+AG GRID REACT DATA PULLING HELPER FUNCTION
 function getData(suppliers) {
     let data = []
     data = suppliers.map((supplier) => {
@@ -21,11 +34,12 @@ function getData(suppliers) {
     return data
 
 }
-
+*/
 
 
 const Suppliers = ({suppliers}) => {
-
+    /*
+    AG GRID REACT 
     const rowData = getData(suppliers)
     //console.log(rowData);
 
@@ -37,20 +51,62 @@ const Suppliers = ({suppliers}) => {
     { field: "balance" },
     { field: "paymentDate" }
     ])
-    /*
-    """To be included if the Ag Grid React situation doesnt fit the use case later on"""
-        suppliers.map((supplier, i) => 
-            <Supplier key={"Supplier" + supplier.description} supplier={supplier} />
-        )
-    */
-    return (
-        
+
+    
         <div className="ag-theme-alpine" style={{height: 400, width: 1200}}>
             <AgGridReact
                 rowData={rowData}
                 columnDefs={columnDefs}>
             </AgGridReact>
         </div>
+    */
+    /*
+    """To be included if the Ag Grid React situation doesnt fit the use case later on"""
+        suppliers.map((supplier, i) => 
+            <Supplier key={"Supplier" + supplier.description} supplier={supplier} />
+        )
+    */
+
+    
+    return (
+        
+        <TableContainer>
+            <Table variant='simple'>
+                <TableCaption>Supplier Catalog</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>Supplier</Th>
+                        <Th>Supplier ID</Th>
+                        <Th>Location</Th>
+                        <Th>Deposit</Th>
+                        <Th>Balance</Th>
+                        <Th>Payment Date</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {suppliers.map((supplier, i) => 
+                    <Tr>
+                        <Td>{supplier.description}</Td>
+                        <Td>{supplier.supplierID}</Td>
+                        <Td>{supplier.description}</Td>
+                        <Td>{supplier.paymentTerms.deposit}</Td>
+                        <Td>{supplier.paymentTerms.balance}</Td>
+                        <Td>{supplier.paymentTerms.daysAfterPickup}</Td>
+                    </Tr>
+                    )}
+                </Tbody>
+                <Tfoot>
+                    <Tr>
+                        <Th>Supplier</Th>
+                        <Th>Supplier ID</Th>
+                        <Th>Location</Th>
+                        <Th>Deposit</Th>
+                        <Th>Balance</Th>
+                        <Th>Payment Date</Th>
+                    </Tr>
+                </Tfoot>
+            </Table>
+        </TableContainer>
 
     )
 }
