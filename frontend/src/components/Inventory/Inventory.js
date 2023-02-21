7f8888e9135c2dfceebd801830d1c59a82948b50
Liam C <thecouchcushen@gmail.com>
2023-02-20 19:40:19 -0500

    Warehouses & Inventory  separated and use ChakraUI

--- frontend/src/components/Inventory/Inventory.js
+++ frontend/src/components/Inventory/Inventory.js
@@ -1,10 +1,88 @@
 import Warehouse from "./Warehouse"
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Button,
+    ButtonGroup,
+  } from '@chakra-ui/react'
+import { useState } from "react"
+
+/*
+inventory.map((warehouse, i) => <Warehouse key={"Warehouse" + warehouse.warehouseID} warehouse={warehouse} /> )
+*/
 
 const Inventory = ({inventory}) => {
-    return (
-        inventory.map((warehouse, i) => <Warehouse key={"Warehouse" + warehouse.warehouseID} warehouse={warehouse} />
+
+    const [displayWarehouseDetails, setDisplayWarehouseDetails] = useState(false)
+    const [currentWarehouse, setCurrentWarehouse] = useState(inventory[0])
+
+    function buttonPressed (warehouse) {
+        setDisplayWarehouseDetails(true)
+        setCurrentWarehouse(warehouse)
+    }
+
+    if (displayWarehouseDetails === false) {
+        return (
+            <TableContainer>
+                <Table variant='simple'>
+                    <TableCaption>Warehouses</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>Warehouse Description</Th>
+                            <Th>Warehouse ID</Th>
+                            <Th>Location</Th>
+                            <Th>Inventory Details</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {inventory.map((warehouse, i) => 
+                        <Tr key={warehouse.description + "inventory"}>
+                            <Td>{warehouse.description}</Td>
+                            <Td>{warehouse.warehouseID}</Td>
+                            <Td>Location</Td>
+                            <Td><Button colorScheme='blue' onClick={() => buttonPressed(warehouse)}>Inventory Levels</Button></Td>
+                        </Tr>
+                        )}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+            
+            )
+    } else if (displayWarehouseDetails === true) {
+        return (
+            <div>
+                <Button colorScheme='blue' onClick={() => setDisplayWarehouseDetails(false)}>Back to warehouse catalog</Button>
+                <TableContainer>
+                    <Table>
+                        <TableCaption>{currentWarehouse.description} Inventory Levels</TableCaption>
+                        <Thead>
+                            <Tr>
+                                <Th>SKU</Th>
+                                <Th>Quantity</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {currentWarehouse.inventoryLevels.map((inventoryItem, i) => 
+                            <Tr key={currentWarehouse.description + inventoryItem.SKU + "inventory"}>
+                                <Td>{inventoryItem.SKU}</Td>
+                                <Td>{inventoryItem.quantity}</Td>
+                            </Tr>
+                            )}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            </div>
         )
-    )
+    }
+    
+    
 }
 
 export default Inventory
